0ca7bfe1c7060a5564226030d236fbe9ba2f404b
fernandoareias <nando.calheirosx@gmail.com>
2022-09-20 21:43:18 -0300

    Implementação parcial do menu mobile

--- script.js
+++ script.js
@@ -14,19 +14,47 @@ links.forEach((link) => {
   link.addEventListener('click', scrollToSection);
 });
 
-// const sections = document.querySelectorAll('.js-scroll');
-// const windowMetade = window.innerHeight * 0.6;
-// function animationScroll() {
-//   sections.forEach((section) => {
-//     const sectionTop = section.getBoundingClientRect().top - windowMetade;
-//     if (sectionTop < 0) {
-//       section.classList.add('ativo');
-//     }
-//   });
-// }
+/* OUTSIDE CLICK*/
 
-// window.addEventListener('scroll', animationScroll);
+function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
 
-if (screen.width < 1085) {
-  window.alert('Vou terminar de desenvolver a versão mobile nas férias :D');
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      setTimeout(() => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      });
+    });
+
+    element.setAttribute(outside, '');
+  }
+
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
+    }
+  }
 }
+
+/* */
+
+const menuButton = document.querySelector('[data-menu="button"]');
+const menuLista = document.querySelector('[data-menu="list"]');
+
+function openMenu(event) {
+  menuLista.classList.add('active');
+  menuButton.classList.add('active');
+
+  outsideClick(menuLista, ['click', 'touchstart'], () => {
+    menuLista.classList.remove('active');
+    menuButton.classList.remove('active');
+  });
+}
+
+menuButton.addEventListener('click', openMenu);
